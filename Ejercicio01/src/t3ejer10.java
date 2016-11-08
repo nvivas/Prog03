fd48efc3c685b7fa22cc52e651e0485c92bbbe4b
Ignacio Vivas <moeshe@gmail.com>
2016-11-08 20:14:41 +0100

    Ejercicios tema 3

---
+++ Ejercicio01/src/t3ejer10.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class t3ejer10 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner (System.in);
+		
+		int n,i,p=0,j,num;
+		System.out.print("Introduce un número: ");
+		n=teclado.nextInt();
+		num = n;
+		
+		while(p == 0){
+			j = 0;
+			
+			for(i=1;i<=n;i++){
+				
+				if(n % i == 0){
+					j++;
+				}
+				
+			}// fin for
+			if(j == 2){
+				System.out.println("El primo por exceso de " + num + " es " + n);
+				p = 1;
+			
+		}// fin while
+			n = n + 1;
+		
+			
+		
+		
+
+	}
+	}
+}
